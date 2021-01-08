9169108fd0a216bb056a3d1ec8beefade1d633a3
MarcosGraña <marcos.grana@udc.es>
2021-01-08 01:13:31 +0100

    Archivador Simple Test : Detectados 3 bugs

--- src/test/java/gal/udc/fic/vvs/email/archivador/ArchivadorSimpleTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/ArchivadorSimpleTest.java
@@ -1,13 +1,15 @@
 package gal.udc.fic.vvs.email.archivador;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Vector;
 
 import org.junit.Test;
 
-import gal.udc.fic.vvs.email.correo.Carpeta;
+import gal.udc.fic.vvs.email.archivo.Texto;
 import gal.udc.fic.vvs.email.correo.Correo;
+import gal.udc.fic.vvs.email.correo.Mensaje;
 
 
 // Test para archivador simple
@@ -15,80 +17,296 @@ public class ArchivadorSimpleTest {
 	
 	// Declaramos constantes para usar en los tests
 	
-	public static final String nombreArchivadorTest = "archivadorTest";
+	public static final String nombreArchivadorTest = 
+			"archivadorTest";
 	public static final int espacioTest = 20;
+	public static final int espacioVacioTest = 0;
+	public static final int espacioUnicoTest = 1;
 	
 	
-	// Test para comprobar si funciona correctamente o constructor
-		@Test
-	    public void archivadorSimpleTest() {
-			
-			ArchivadorSimple archivador = new ArchivadorSimple(nombreArchivadorTest, espacioTest);	
-			
-			Vector vector = new Vector();
-			
-	        assertEquals(nombreArchivadorTest, archivador.obtenerNombre());
-	        assertEquals(espacioTest, archivador.obtenerEspacioDisponible());
-	        assertEquals(espacioTest, archivador.obtenerEspacioTotal());
-	        
-	    }
-	
-
-	// Test para comprobar si o método recupera o nome posto no constructor
+	// Comprobamos que o constructor non devolva nulo
 	@Test
-    public void obtenerNombreTest() {
+    public void archivadorSimpleNonNuloTest() {
 		
-		ArchivadorSimple archivador = new ArchivadorSimple(nombreArchivadorTest, espacioTest);	
-		String nombreReal = archivador.obtenerNombre();
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);			
 		
-        assertEquals(nombreArchivadorTest, nombreReal);
+        assertTrue(archivador != null);
+        
+    }
+
+		
+	// Comprobamos que o constructor determina correctamente o
+		// nome do archivador pasado por parámetros
+	@Test
+    public void archivadorSimpleNombreTest() {
+		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		
+		String nombreObj = archivador.obtenerNombre();
+		
+        assertEquals(nombreArchivadorTest, nombreObj);
     }
 	
-	// Test para comprobar si o método recupera o espacio total correctamente establecido no constructor
+
+	// Test para comprobar si o constructor determina o espazo total
+		//   correctamente pasado por parámetros
 	@Test
-    public void obtenerEspacioTotalTest() {
+    public void archivadorSimpleEspazoTotalTest() {
 		
-		ArchivadorSimple archivador = new ArchivadorSimple(nombreArchivadorTest, espacioTest);	
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
 		int espacioTotal = archivador.obtenerEspacioTotal();
 		
         assertEquals(espacioTest, espacioTotal);
     }
 	
-	// Test para comprobar si o método recupera o espacio disponible correctamente establecido no constructor
+	// Test para comprobar si o constructor determina o espazo
+		//   disponible, que deberá ser igual ao espazo inicial
+		// xa que non é posible que se ocupara con ningún mensaxe
+		// no momento actual
 	@Test
-    public void obtenerEspacioDisponibleTest() {
+    public void archivadorSimpleEspazoDisponibleTest() {
 		
-		ArchivadorSimple archivador = new ArchivadorSimple(nombreArchivadorTest, espacioTest);	
-		int espacioDisponible = archivador.obtenerEspacioDisponible();
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		int espacioDisponible = 
+				archivador.obtenerEspacioDisponible();
 		
         assertEquals(espacioTest, espacioDisponible);
     }
 	
-	// Test para comprobar si o método obtén delegado a null
-		@Test
-	    public void obtenerDelegadoTest() {
-			
-			ArchivadorSimple archivador = new ArchivadorSimple(nombreArchivadorTest, espacioTest);	
-			Archivador nulo = archivador.obtenerDelegado();
-			
-	        assertEquals(null, nulo);
-	    }
+	// Comprobamos que o método obtenerNombre devolve correctamente o
+	// nome do archivador pasado por parámetros ao constructor
+	@Test
+	public void archivadorSimpleObtenerNombreTest() {
 		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
 		
-		// Test para comprobar si almacena correo sobre _correos vacío y actualiza correctamente
-		@Test
-	    public void almacenarCorreoTest() {
-			
-			ArchivadorSimple archivador = new ArchivadorSimple(nombreArchivadorTest, espacioTest);	
-			Correo carpeta = new Carpeta("carpeta");
-			
-			boolean insertado = archivador.almacenarCorreo(carpeta);
-			
-	        assertEquals(espacioTest - 1 , archivador.obtenerEspacioDisponible());
-	        assertEquals(espacioTest , archivador.obtenerEspacioTotal());
-	        assertEquals(true , insertado);
-	    }		
+		String nombreObj = archivador.obtenerNombre();
 		
-		// Faltan más tests después de notificar el bug
+	    assertEquals(nombreArchivadorTest, nombreObj);
+	}
+	
+	// Test que comprueba que co espacio dispoñible suficiente
+	//   o método devolve true
+	@Test
+    public void almacenarCorreoConSuficienteEspacioDevuelveTrueTest() {
+		
+		ArchivadorSimple archivador = new ArchivadorSimple(
+				nombreArchivadorTest, espacioTest);	
+		
+		// Creamos el Correo para almacenar
+		Texto texto = new Texto("nombre","contenido");
+		Correo mensaje = new Mensaje(texto);
+		
+		boolean insertado = archivador.almacenarCorreo(mensaje);
+
+        assertEquals(true , insertado);
+    }	
+	
+	
+	// Test que comprueba que sin espacio dispoñible suficiente
+	//   o método devolve false
+	@Test
+    public void almacenarCorreoSinSuficienteEspacioDevuelveTrueTest() {
+		
+		ArchivadorSimple archivador = new ArchivadorSimple(
+				nombreArchivadorTest, espacioVacioTest);	
+		
+		// Creamos el Correo para almacenar
+		Texto texto = new Texto("nombre","contenido");
+		Correo mensaje = new Mensaje(texto);
+		
+		boolean insertado = archivador.almacenarCorreo(mensaje);
+
+        assertEquals(false , insertado);
+    }		
+	
+	// Test que comprueba que cun só espacio dispoñible suficiente
+	//   o método devolve true a primeira vez lanzado
+	@Test
+    public void almacenarCorreoCunEspacioUnicoDevuelveTrueTest() {
+		
+		ArchivadorSimple archivador = new ArchivadorSimple(
+				nombreArchivadorTest, espacioUnicoTest);	
+		
+		// Creamos el Correo para almacenar (1)
+		Texto texto1 = new Texto("nombre 1","contenido 1");
+		Correo mensaje1 = new Mensaje(texto1);
+		
+		boolean insertado1 = archivador.almacenarCorreo(mensaje1);
+
+        assertEquals(true , insertado1);
+    }	
+	
+	// Test que comprueba que cun só espacio dispoñible suficiente
+	//   o método devolve false na segunda inserción
+	@Test
+    public void almacenarCorreoCunEspacioUnicoDosVecesDevuelveFalseTest() {
+		
+		ArchivadorSimple archivador = new ArchivadorSimple(
+				nombreArchivadorTest, espacioUnicoTest);	
+		
+		// Creamos el Correo para almacenar (1)
+		Texto texto1 = new Texto("nombre 1","contenido 1");
+		Correo mensaje1 = new Mensaje(texto1);
+		
+		// Creamos el Correo para almacenar (2)
+		Texto texto2 = new Texto("nombre 2","contenido 2");
+		Correo mensaje2 = new Mensaje(texto2);
+		
+		archivador.almacenarCorreo(mensaje1);
+		boolean insertado = archivador.almacenarCorreo(mensaje2);
+
+        assertEquals(false , insertado);
+    }	
+	
+	// Test que comproba que o metodo almacenar correo resta correctamente
+	//		o espazo dispoñible	
+	@Test
+    public void almacenarCorreoActualizaEspazoDisponibleTest() {
+		
+		ArchivadorSimple archivador = new ArchivadorSimple(
+				nombreArchivadorTest, espacioTest);	
+		
+		// Creamos el Correo para almacenar
+		Texto texto = new Texto("nombre","contenido");
+		Correo mensaje = new Mensaje(texto);
+		
+		archivador.almacenarCorreo(mensaje);
+		
+		int espazoCorrecto = espacioTest - mensaje.obtenerTamaño();
+
+        assertEquals(espazoCorrecto , archivador.obtenerEspacioDisponible());
+    }	
+	
+	// Test que comproba que o metodo almacenar correo non resta espazo
+	//   si non é posible insertar o correo	
+	@Test
+    public void almacenarCorreoNonActualizaEspazoDisponibleTest() {
+		
+		ArchivadorSimple archivador = new ArchivadorSimple(
+				nombreArchivadorTest, espacioVacioTest);	
+		
+		// Creamos el Correo para almacenar
+		Texto texto = new Texto("nombre","contenido");
+		Correo mensaje = new Mensaje(texto);
+		
+		archivador.almacenarCorreo(mensaje);
+		
+		int espazoCorrecto = espacioVacioTest;
+
+        assertEquals(espazoCorrecto , archivador.obtenerEspacioDisponible());
+    }	
+			
+			
+	// Test para comprobar si metodo devolve o espazo total
+	//   correctamente pasado por parámetros
+	@Test
+    public void archivadorSimpleObterEspazoTotalTest() {
+		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		int espacioTotal = archivador.obtenerEspacioTotal();
+		
+        assertEquals(espacioTest, espacioTotal);
+    }
+		
+	// Test para comprobar si o método devolve o espazo disponible
+	//	correctamente, que será equivalente ao espazo total
+	//  se non se añade nigún correo
+	@Test
+    public void archivadorSimpleObterEspazoDisponibleTest() {
+		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		int espacioDisponible = 
+				archivador.obtenerEspacioDisponible();
+		
+        assertEquals(espacioTest, espacioDisponible);
+    }
+
+	// Test para comprobar que se non se añadeu ningún delegado,
+	//  o metodo obter delegado debería devolver null
+	@Test
+	public void archivadorSimpleObterDelegadoNullTest() {
+		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		
+		Archivador delegado = archivador.obtenerDelegado();
+		
+		assertEquals(null, delegado);
+		
+	}
+	
+	// Test para comprobar que si se añadeu algún delegado,
+	//  o metodo obter delegado non debería devolver null
+	@Test
+	public void archivadorSimpleObterDelegadoAñadidoTest() {
+		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		
+		ArchivadorSimple archivadorDelegado = 
+				new ArchivadorSimple(nombreArchivadorTest + "X", 
+						espacioTest + 10);	
+		
+		archivador.establecerDelegado(archivadorDelegado);
+		
+		Archivador delegado = archivador.obtenerDelegado();
+		
+		assertTrue( null != delegado.obtenerDelegado());
+		
+	}
+	
+	
+	// Test para comprobar que si se añadeu algún delegado,
+	//  o metodo obter delegado non debería devolver null
+	@Test
+	public void archivadorSimpleObterDelegadoDistintoNullTest() {
+		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		
+		Archivador delegado = archivador.obtenerDelegado();
+		
+		assertEquals(null, delegado);
+		
+	}
+	
+	// Test para comprobar que si se añadeu algún delegado,
+	//  o metodo obter delegado non debería devolver null
+	@Test
+	public void archivadorSimpleEstablecerDelegadoTest() {
+		
+		ArchivadorSimple archivador = 
+				new ArchivadorSimple(nombreArchivadorTest, 
+						espacioTest);	
+		
+		ArchivadorSimple archivadorDelegado = 
+				new ArchivadorSimple(nombreArchivadorTest + "X", 
+						espacioTest + 10);	
+		
+		archivador.establecerDelegado(archivadorDelegado);
+		
+		Archivador delegado = archivador.obtenerDelegado();
+		
+		assertTrue( null != delegado.obtenerDelegado());
+		
+	}
 	
 }
