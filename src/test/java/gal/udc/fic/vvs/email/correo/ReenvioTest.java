cb16be5998bd145d709ab1115d9fdbd1157f7024
Marcos Graña Domínguez <marcos.grana@udc.es>
2021-01-13 21:36:02 +0100

    Test Reenvio : Bug encontrado en obterVisualizacion

---
+++ src/test/java/gal/udc/fic/vvs/email/correo/ReenvioTest.java
@@ -0,0 +1,66 @@
+package gal.udc.fic.vvs.email.correo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import gal.udc.fic.vvs.email.archivo.Texto;
+
+public class ReenvioTest {
+	
+	public static final String NOMBRE1 = "nombre1";
+	public static final String NOMBRE2 = "nombre2";
+	public static final String CONTENIDO1 = "contenido1";
+	public static final String CONTENIDO2 = "contenido2";
+	
+	public static final Texto TEXTO1 = new Texto(NOMBRE1, CONTENIDO1);
+	public static final Texto TEXTO2 = new Texto(NOMBRE2, CONTENIDO2);
+	
+	Mensaje MENSAJE1 = new Mensaje(TEXTO1);
+	Mensaje MENSAJE2 = new Mensaje(TEXTO2);
+	
+	
+///////////////////////////////////// TESTS //////////////////////////////////////////	
+
+	// Probamos que el constructor no devuelva nulo
+	@Test
+	public void constructorReenvioNotNull() {
+		
+		Reenvio reenvio = new Reenvio(MENSAJE1, MENSAJE2);
+		
+		assertNotNull(reenvio);
+		
+	}
+	
+	
+	// Comprobamos que obtener tamaño devuelva el tamaño del mensaje + del propio
+	//		correo
+	@Test
+	public void reenvioObtenerTamaño() {
+		
+		Reenvio reenvio = new Reenvio(MENSAJE1, MENSAJE2);
+		
+		int tamañoEsperado = MENSAJE1.obtenerTamaño() + MENSAJE2.obtenerTamaño();
+		
+		assertEquals(tamañoEsperado, reenvio.obtenerTamaño());
+		
+	}
+	
+	// Comprobamos que obtenerVisualización devuelva la visualización más el String
+	//		"\n\n---- Correo reenviado ----\n\n"
+	@Test
+	public void reenvioObtenerVisualizacion() {
+		
+		Reenvio reenvio = new Reenvio(MENSAJE1, MENSAJE2);
+		
+		String visualizacionEsperada = MENSAJE1.obtenerVisualizacion() +
+				"\n\n---- Correo reenviado ----\n\n";
+		
+		assertEquals(visualizacionEsperada, reenvio.obtenerVisualizacion());
+		
+	}
+	
+	
+
+}
